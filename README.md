1d821367a2696b12760218c94a4e36b21d10f736
royggle <yunoroy@gmail.com>
2023-03-29 20:39:35 +0900

    [INIT] Set Up Zoom Clone Coding Project

---
+++ README.md
@@ -0,0 +1,6 @@
+# Noom
+
+Zoom Clone using NodeJS, WebRTC and Websockets
+
+
+Nodemon.json is available for nodemon settings
---
+++ src/server.js
@@ -0,0 +1,21 @@
+import path from 'path';
+import express from "express";
+import http from "http";
+import WebSocket from 'ws';
+const __dirname = path.resolve();
+const app = express();
+
+app.set("view engine", "pug");
+app.set("views", __dirname + "/src/views");
+app.use("/public", express.static(__dirname + "/src/public"));
+
+app.get("/", (req, res) =>  res.render("home"));
+app.get("/*", (req, res) => res.redirect("/")); // redirect to main page if requested url is unmatched to any routes;
+
+const handleListen = () => console.log(`Listening on http://localhost:3000`);
+
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+
+server.listen(3000, handleListen);
